2b12b90892b4bcd8c1657a7ea812f86dd182418f
mario <mariosp860@gmail.com>
2024-10-01 15:04:53 +0200

    Agregar rutas para los métodos PUT, PATCH y DELETE en ejercicio2

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,42 @@ Route::get('/ejercicio1', function () {
 Route::post('/ejercicio1', function () {
     return "POST OK";
 });
+Route::post('/ejercicio2/a', function (Request $request) {
+    return response()->json([
+        'name' => $request->input('name'),
+        'description' => $request->input('description'),
+        'price' => $request->input('price')
+    ]);
+});
+Route::post('/ejercicio2/b', function (Request $request) {
+    if ($request -> input('price') < 0) {
+        return response()->json(["message" => "Price can't be less than 0"], 422);
+    }
+    return response()->json([
+        'name' => $request->input('name'),
+        'description' => $request->input('description'),
+        'price' => $request->input('price')
+    ]);
+});
+
+Route::post('/ejercicio2/c', function (Request $request) {
+    $price = $request->input('price');
+    $discount = $request->input('discount');
+
+    if ($discount == "SAVE5") {
+        $price = $price * 0.95;
+    } else if ($discount == "SAVE10") {
+        $price = $price * 0.9;
+    } else if ($discount == "SAVE15") {
+        $price = $price * 0.85;
+    } else {
+        return response()->json(["message" => "Invalid discount code"], 422);
+    }
+
+    return response()->json([
+        'name' => $request->input('name'),
+        'description' => $request->input('description'),
+        'price' => $price,
+        'discount' => $discount
+    ]);
+});
